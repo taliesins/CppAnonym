a2b3e0ab5583d5af65f91d43982c6e98b5c52717
Akira Sugiura <urasandesu@gmail.com>
2011-11-13 15:55:18 +0900

    Initial Setup
    CppAnonym is common library to use in C++ project mainly.
    The utilities that is tested in project, CppTroll and will be beneficial is added to above project.

---
+++ CppAnonym/dllmain.h
@@ -0,0 +1,10 @@
+﻿// dllmain.h : モジュール クラスの宣言
+
+class CCppAnonymModule : public CAtlDllModuleT< CCppAnonymModule >
+{
+public :
+	DECLARE_LIBID(LIBID_CppAnonymLib)
+	DECLARE_REGISTRY_APPID_RESOURCEID(IDR_CPPANONYM, "{744CBF35-B24F-4EAC-B473-45CA44F4981E}")
+};
+
+extern class CCppAnonymModule _AtlModule;
---
+++ CppAnonymTest/stdafx.h
@@ -0,0 +1,26 @@
+﻿// stdafx.h : 標準のシステム インクルード ファイルのインクルード ファイル、または
+// 参照回数が多く、かつあまり変更されない、プロジェクト専用のインクルード ファイル
+// を記述します。
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#include <stdio.h>
+#include <tchar.h>
+
+
+#define _ATL_CSTRING_EXPLICIT_CONSTRUCTORS      // 一部の CString コンストラクタは明示的です。
+
+#include <atlbase.h>
+#include <atlstr.h>
+
+// TODO: プログラムに必要な追加ヘッダーをここで参照してください。
+#import "libid:E21F0F69-633F-45E2-BF2E-0EB2D6F2CE6B" version("1.0") no_namespace // CppAnonym 1.0 タイプ ライブラリ
+
+#ifndef URASANDESU_CPPANONYM_CPPANONYMDEPENDSON_H
+#include <Urasandesu/CppAnonym/CppAnonymDependsOn.h>
+#endif
+
+#include <gtest/gtest.h>
