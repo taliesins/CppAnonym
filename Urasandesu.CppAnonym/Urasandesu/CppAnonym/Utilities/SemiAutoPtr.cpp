87a93884b5c9c1ed049dda505f9f2386a90639de
Akira Sugiura <urasandesu@gmail.com>
2012-11-03 08:32:59 +0900

    Compilation Time Reduction Trial: Overwrite SemiAutoPtr.hxx
    Change the files that have the extension .hpp to the files that have the extension .cpp.

--- Urasandesu.CppAnonym/Urasandesu/CppAnonym/Utilities/SemiAutoPtr.cpp
+++ Urasandesu.CppAnonym/Urasandesu/CppAnonym/Utilities/SemiAutoPtr.cpp
@@ -1,6 +1,5 @@
-﻿#pragma once
-#ifndef URASANDESU_CPPANONYM_UTILITIES_SEMIAUTOPTR_HXX
-#define URASANDESU_CPPANONYM_UTILITIES_SEMIAUTOPTR_HXX
+﻿
+#include "stdafx.h"
 
 #ifndef URASANDESU_CPPANONYM_UTILITIES_SEMIAUTOPTR_H
 #include <Urasandesu/CppAnonym/Utilities/SemiAutoPtr.h>
@@ -15,5 +14,3 @@ namespace Urasandesu { namespace CppAnonym { namespace Utilities {
     }   // namespace SemiAutoPtrDetail {
 
 }}}   // namespace Urasandesu { namespace CppAnonym { namespace Utilities {
-
-#endif  // #ifndef URASANDESU_CPPANONYM_UTILITIES_SEMIAUTOPTR_HXX
